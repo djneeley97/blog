055e0ceec924a0e67880eb184c2be29231f041ed
djneeley97 <djneeley97@gmail.com>
2024-06-01 08:49:07 -0700

    113

---
+++ posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+
+    def _str_(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=256)
+    author = models. ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models. ForeignKey(
+        Status,
+        on_delete=models.CASCADE
+    )
+
+    def _str_(self):
+        return self.title
---
+++ posts/views.py
@@ -0,0 +1,38 @@
+
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView
+)
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "author", "body", "status"]
+
+def form_valid(self, form):
+    form. instance.author = self.request.user
+    return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "status"]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse("list")
